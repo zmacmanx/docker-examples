99a4beaf0c5e771dea45340a4a2299be91d4e09b
zmacmanx <zmacmanx@gmail.com>
2018-07-02 09:44:04 -0400

    Adding next chapter

---
+++ WhatIsAContainer/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ANS
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>"
+docker image pull alpine
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo 
+read ANS
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>"
+docker image ls
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo 
+read ANS
