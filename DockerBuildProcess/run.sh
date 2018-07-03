92f295180793d91f4c0baca9082f67bb69c07c7c
Marc Cantelmo <zmacmanx@gmail.com>
2018-07-03 13:40:17 -0400

    Adding new section

---
+++ DockerBuildProcess/run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+clear
+which docker > /dev/null 2>&1
+
+if [ ${?} -eq 0 ]
+then
+	docker info > /dev/null 2>&1
+	if [ ${?} -eq 0 ]
+	then
+		echo "--------------------------"
+		echo ">> Create a index.js file with the following:"
+		echo 'var os = require("os");'
+		echo 'var hostname = os.hostname("");'
+		echo 'console.log("Hello from " + hostname);'
+		echo 
+		echo "<<< Press return to continue >>> "
+		read ANS
+	
+		echo "--------------------------"
+		echo ">> Create a Dockerfile with the following:"
+		echo 'FROM alpine'
+		echo 'RUN apk update && apk add node.js'
+		echo 'COPY . /app'
+		echo 'WORKDIR /app'
+		echo 'CMD ["node", "index.js"]'
+		echo 
+		echo "<<< Press return to continue >>> "
+		read ANS
+	
+		echo "--------------------------"
+		echo ">> docker image build -t hello.v0.1 ."
+		docker image build -t hello.v0.1 .
+		echo 
+		echo "<<< Press return to continue >>> "
+		read ANS
+	else
+		echo "Docker is not running on this system"
+	fi
+else
+	echo "Docker does not seem to be setup for this environment,  Please make sure docker is installed and in your path"
+fi
