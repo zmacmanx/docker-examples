7755c6ce25ff410dfedf0c18e33a08dee615f137
Marc Cantelmo <zmacmanx@gmail.com>
2018-07-03 11:35:23 -0400

    Adding a clean up

--- ContainerIsolation/run.sh
+++ ContainerIsolation/run.sh
@@ -8,6 +8,7 @@ then
 	docker info > /dev/null 2>&1
 	if [ ${?} -eq 0 ]
 	then
+		echo "y" | docker system prune > /dev/null 2>&1
 		docker rmi -f `docker images -aq` >/dev/null 2>&1
 
 		echo "--------------------------"
@@ -20,6 +21,87 @@ then
 		echo
 		echo "<<< Press return to continue >>> "
 		read ANS
+		echo
+
+		echo "--------------------------"
+		echo ">> ls -l"
+		echo
+		ls -l
+		echo
+		echo "<<< Press return to continue >>> "
+		read ANS
+		echo
+
+		echo "--------------------------"
+		echo ">> docker container run alpine ls -l"
+		echo
+		docker container run alpine ls -l
+		echo
+		echo "<<< Press return to continue >>> "
+		read ANS
+		echo
+
+		echo "--------------------------"
+		echo ">> docker container ls -a"
+		echo
+		docker container ls -a
+		CONTAINER_ID=`docker container ls -a | grep "/bin/sh" | awk '{ print $1 }'`
+		echo
+		echo "<<< Press return to continue >>> "
+		read ANS
+		echo
+
+		echo "--------------------------"
+		echo "Using Container ID: ${CONTAINER_ID}"
+		echo
+		echo ">> docker container start ${CONTAINER_ID}"
+		echo
+		docker container start ${CONTAINER_ID}
+		echo
+		echo "<<< Press return to continue >>> "
+		read ANS
+		echo
+
+		echo "--------------------------"
+		echo "Container ID: ${CONTAINER_ID}"
+		echo
+		echo ">> docker container ls"
+		echo
+		docker container ls 
+		echo
+		echo "<<< Press return to continue >>> "
+		read ANS
+		echo
+
+		echo "--------------------------"
+		echo
+		echo ">> docker container exec ${CONTAINER_ID} ls"
+		echo
+		docker container exec ${CONTAINER_ID} ls
+		echo
+		echo "<<< Press return to continue >>> "
+		read ANS
+		echo
+
+		echo "--------------------------"
+		echo
+		echo ">> docker diff ${CONTAINER_ID}"
+		echo
+		docker diff ${CONTAINER_ID}
+		echo
+		echo "<<< Press return to continue >>> "
+		read ANS
+		echo
+
+		echo "--------------------------"
+		echo
+		echo ">> docker inspect ${CONTAINER_ID}"
+		echo
+		docker inspect ${CONTAINER_ID}
+		echo
+		echo "<<< Press return to continue >>> "
+		read ANS
+		echo
 	else
 		echo "Docker is not running on this system"
 	fi
