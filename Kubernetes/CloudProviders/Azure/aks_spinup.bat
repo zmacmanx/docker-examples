8cbbd2098c70fc1f2be15fa60f3c35ee1399d04b
Marc A Cantelmo <32905699+zmacmanx@users.noreply.github.com>
2018-07-05 13:58:28 -0400

    Updating the master branch (#19)
    * Adding readme for the introduction section
    
    * Adding details about image
    
    * Updating script flow and explanation text
    
    * Adding new sections to the document

---
+++ Kubernetes/CloudProviders/Azure/aks_spinup.bat
@@ -0,0 +1,59 @@
+#
+# Register the container service for use.
+# az provider show -n Microsoft.ContainerService will show register
+#
+az provider register -n Microsoft.ContainerService
+
+# Create a resource group to contain all the entities going forward
+# Using the Eastern United States datacenter
+#
+az group create --name rg-kubedemo-aks --location eastus
+
+#
+# The following with establish the cluster name and size with the type of keys
+# needed to communicate between the entities.  Node the resource group is being 
+# used to contain all the billing and components together.
+#
+az aks create --resource-group rg-kubedemo-aks --name kc-kubedemo-aks --node-count 1 --generate-ssh-keys
+
+#
+# Install the package for command line control of the kube cluster
+#
+az aks install-cli
+
+#
+#
+#
+ak aks get-credentials --resource-group rg-kubedemo-aks --name kc-kubedemo-aks
+
+#
+#
+#
+kubectl get cs
+kubectl cluster-info
+
+kubectl create -f azure_vote.yml
+kubectl get po --watch
+kubectl get srv --watch
+
+kubectl run --image nginx myweb
+kubectl get deploy
+kubectl get pods
+
+#
+# Need the name of the pod that you get from kubectl get pods.  You are going to use that
+# name to open access to the application through the LoadBalancer.
+#
+kubectl expose pod myweb-190417145-0sgv4 --port 80 --target-port 80 --type LoadBalancer
+
+# List services running on the cluster
+kubectl get svc
+
+az aks scale --resource-group <resource group> --name <cluster name> --node-count <node count>
+
+# Upgrade the cluster version
+#
+az aks upgrade --resource-group <resource group> --name <cluster name> --kubernetes-version <kubernetes version>
+
+
+
--- WhatIsAImage/run.sh
+++ WhatIsAImage/run.sh
@@ -8,30 +8,45 @@ then
 	docker info > /dev/null 2>&1
 	if [ ${?} -eq 0 ]
 	then
+		clear
 		echo "--------------------------"
+		echo "First we need to clean all existing images that may exist from running containers in previous sessions.  "
+		echo "The commands that follow will clear all images and containers from the list: "
+		echo ""
 		echo ">> docker system prune"
-		echo ">> docker rmi -f `docker images -aq`"
+		echo ">> docker rmi -f \`docker images -aq\`"
 		echo "y" | docker system prune > /dev/null 2>&1
 		docker rmi -f `docker images -aq` >/dev/null 2>&1
 		echo
 		echo "<<< Press return to continue >>> "
 		read ANS
 
+		clear
 		echo "--------------------------"
+		echo "Docker uses images to run processes.  Put simply images are programs/executibles that are self-contained and "
+		echo "can be run.  A list of these images."
 		echo ">> docker image ls"
 		docker image ls
 		echo
 		echo "<<< Press return to continue >>> "
 		read ANS
 
+		clear
 		echo "--------------------------"
+		echo "Now that we have a clean environment, lets pull an image called alpine.  Alpine is a small linux footprint to "
+		echo "run commands.  Think of the pull the command as a search to find an item and put it in your shopping cart. "
+		echo "In this case it is an executiable known as an image and the running of it is a container "
 		echo ">> docker image pull alpine"
 		docker image pull alpine
 		echo
 		echo "<<< Press return to continue >>> "
 		read ANS
 	
+		clear
 		echo "--------------------------"
+		echo "The image was found and the pulled down to the local repository with all associated checksums ready to be run "
+		echo "In the next section we will show you have to run the image, but for now notice that the ls command will show "
+		echo "all alpine image and associate information "
 		echo ">> docker image ls"
 		docker image ls
 		echo
